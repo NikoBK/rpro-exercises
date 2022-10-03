38458069060f515116aa90712d9aa399efb58662
Nikolaj Bjørnager Krebs <NikoBK@users.noreply.github.com>
2022-10-03 20:23:14 +0200

    course 1 exercises & notes

---
+++ Course1/excercise01.ino
@@ -0,0 +1,26 @@
+/*
+   Excercise01.ino
+
+    Created on: Oct 3, 2022,
+    Author: Nikolaj Krebs
+*/ 
+
+/* Summary:
+    Simple code based on the blink example code. This code
+    blinks pin 17, at every 250th milliseconds.
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(17, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(17, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(250);                       // wait for a second
+  digitalWrite(17, LOW);    // turn the LED off by making the voltage LOW
+  delay(250);                       // wait for a second
+}
+
---
+++ Course1/excercise02.ino
@@ -0,0 +1,66 @@
+/*
+   Excercise02.ino
+
+    Created on: Oct 3, 2022,
+    Author: Nikolaj Krebs
+*/ 
+
+int onTimeMS = 2000; // How long each LED is on (in milliseconds). Set to 2 seconds.
+
+// LED PINS
+int leftIRIndicator = 5;
+int rightIRIndicator = 19;
+int txLed = 30;
+int rxLed = 17;
+int yellowLed = 13; // This is also LED_BUILTIN.
+
+// The setup function runs once when you press reset or power the board.
+void setup()
+{
+    // Setting all the following pins to OUTPUT mode, so we can write to them.
+    pinMode(leftIRIndicator, OUTPUT);
+    pinMode(rightIRIndicator, OUTPUT);
+    pinMode(txLed, OUTPUT);
+    pinMode(rxLed, OUTPUT);
+    pinMode(yellowLed, OUTPUT);
+}
+
+// The loop function runs over and over again forever.
+void loop() 
+{
+    // Turn the left IR Indicator LED on and off.
+    digitalWrite(rightIRIndicator, LOW);
+    digitalWrite(leftIRIndicator, HIGH);
+    delay(onTimeMS);
+    digitalWrite(rightIRIndicator, HIGH);
+    digitalWrite(leftIRIndicator, LOW);
+    delay(250); // This is just a delay to move on to the next LED, no need to change this.
+
+    // Turn the right IR Indicator LED on and off (basically the same as the above code).
+    digitalWrite(rightIRIndicator, HIGH);
+    digitalWrite(leftIRIndicator, HIGH);
+    delay(onTimeMS);
+    digitalWrite(rightIRIndicator, LOW);
+    digitalWrite(leftIRIndicator, LOW);
+    delay(250);
+
+    // Turn the RX LED ( The red one ) on and off.
+    digitalWrite(rxLed, LOW);
+    delay(onTimeMS);
+    digitalWrite(rxLed, HIGH);
+    delay(250);
+
+    // Turn the TX LED ( The green one ) on and off.
+    digitalWrite(txLed, LOW);
+    delay(onTimeMS);
+    digitalWrite(txLed, HIGH);
+    delay(250);
+
+    // Turn the yellow LED on and off.
+    digitalWrite(yellowLed, HIGH);
+    delay(onTimeMS);
+    digitalWrite(yellowLed, LOW);
+    delay(250);
+
+    // At this point it will loop up back to the top of loop and restart the LED carousel.
+}
