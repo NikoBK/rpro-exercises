971d6b0a108ae6e24283062080d7a7b04ad0a689
Nikolaj Bjørnager Krebs <NikoBK@users.noreply.github.com>
2022-10-05 17:59:01 +0200

    course 02, exercise 3

--- course2/exercise3.ino
+++ course2/exercise3.ino
@@ -3,15 +3,31 @@
 
     Created on: Oct 5, 2022,
     Author: Nikolaj Krebs,
-    Course 02, Exercise 1 - NAME, Friday Sep. 23
+    Course 02, Exercise 3 - Random numbers, Friday Sep. 23
 */ 
+
+int randomMinimum = 200; // The smallest random number we can get.
+int randomMaximum = 1000; // The largest random number we can get.
+int randomDelay; // We will set this value in the loop.
+
+int yellowLED = 13; // The LED on pin13.
 
 void setup()
 {
+  // if you do not know what this does at this point, 
+  // go back to exercise 1 and 2 and read the comments.
+  Serial.begin(9600);
 
+  // Make sure we can write to pin13.
+  pinMode(yellowLED, OUTPUT);
 }
 
 void loop()
 {
-
+    randomDelay = random(randomMinimum, randomMaximum); // This will be a different value everytime loop starts over.
+    Serial.println("Blinking period = " + (String)randomDelay); // Print the randomly generated delay time.
+    digitalWrite(yellowLED, HIGH); // Turn the LED on.
+    delay(randomDelay); // Leave it on for the random generated amount of time.
+    digitalWrite(yellowLED, LOW); // Turn it off after the time has expired.
+    delay(randomDelay); // Wait for the same randomized time before generating a new random time and doing all this again.
 }
