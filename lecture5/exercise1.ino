36c7f0b43e52ca0c20a08508712bbb43c3dec00f
Nikolaj Bjørnager Krebs <NikoBK@users.noreply.github.com>
2022-11-12 00:11:20 +0100

    lecture 5, exercise 1

--- lecture5/exercise1.ino
+++ lecture5/exercise1.ino
@@ -6,7 +6,8 @@
     Lecture 5, Exercise 1 - Simple functions, Friday Oct. 28 (moved to Friday Nov. 4)
 */ 
 
-
+int values[] = { 1, 10, 100, 1000, 10000 };
+String measures[] = { "nm", "mm", "cm", "m", "km" };
 
 void setup()
 {
@@ -15,5 +16,41 @@ void setup()
 
 void loop()
 {
+  // We know both our values & measures arrays have the size of 5.
+  for (int i = 0; i < 5; i++) {
+    // 'i' represents the position of which we read the arrays in at 
+    // a given time.
+    Serial.println("Reading arrays on position: " + (String)i);
+    readValues(values, measures, i);
+  }
 
+  delay(1000);
+}
+
+// Our greeting function that greets the user through the serial monitor.
+void greeting() {
+  Serial.println("Hello World!");
+}
+
+// Our value print function, that prints a value of parameter type int, representing x units, in 
+// measurement units represented by the parameter type of String.
+void printValue(int value, String measure) {
+  // Say that int value is: 3, and String measure is "mm".
+  // The print will be: The given parameters are: 3 mm.
+  Serial.println("The given parameters are: " + (String)value + measure);
+}
+
+// Used to read given arrays, in this exercise a int array followed by a String array.
+// The 'pos' parameter of type int, is used to define the reading position in both arrays
+// at the same time.
+void readValues(int values[], String measures[], int pos) {
+
+  // Grab the array entries from the given position.
+  // NOTE: If the value of parameter 'pos' is any number higher than
+  // the array sizes, things will break.
+  int value = values[pos];
+  String measure = measures[pos];
+
+  // Say the line, Bart!
+  printValue(value, measures);
 }
