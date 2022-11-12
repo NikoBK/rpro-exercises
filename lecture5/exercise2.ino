b8f2947203ccacb38e6fcead9c76ed62e2708707
Nikolaj Bjørnager Krebs <NikoBK@users.noreply.github.com>
2022-11-12 20:04:05 +0100

    lecture 5, exercise 2

--- lecture5/exercise2.ino
+++ lecture5/exercise2.ino
@@ -6,7 +6,7 @@
     Lecture 5, Exercise 2 - Scope of the variables., Friday Oct. 28 (moved to Friday Nov. 4)
 */ 
 
-
+int myCounterGlobal = 0;
 
 void setup()
 {
@@ -15,5 +15,23 @@ void setup()
 
 void loop()
 {
+  int iterations = 5;
+  int myCounterLocal = 0;
 
+  for (int i = 0; i < iterations; i++) {
+    Serial.println("-------------------------------");
+    Serial.println("myCounterGlobal value is currently: " + (String)myCounterGlobal + ". We are adding: " + (String)i + " to myCounterGlobal.");
+    Serial.println("myCounterLocal value is currently: " + (String)myCounterLocal + ". We are adding: " + (String)i + " to myCounterLocal.");
+    increment(i, myCounterGlobal);
+    increment(i + 1, myCounterLocal);
+    Serial.println("myCounterGlobal has been incremented with: " + (String)i + ". myCounterGlobal is now: " + (String)myCounterGlobal);
+    Serial.println("myCounterLocal has been incremented with: " + (String)i + ". myCounterLocal is now: " + (String)myCounterLocal);
+    delay(4500);
+  }
+}
+
+// Adds whatever value of the parameter 'inc' of type int is, to 'counter'.
+void increment(int inc, int &counter)
+{
+  counter += inc;
 }
